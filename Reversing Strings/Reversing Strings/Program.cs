2d2bb9b930fdc015cd8f61f85e29e70fd3cd9247
2hands <denueg@yahoo.com>
2014-01-02 21:34:48 -0500

    Reversing String
    Reversing String project

---
+++ Reversing Strings/Reversing Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reversing_Strings
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string to be reversed");
+            string str = Console.ReadLine();
+            Console.WriteLine("String length is: " + str.Length);
+            int x = str.Length - 1;
+            int y = str.Length;
+            Console.Write("The reversed string is: ");
+            for (int i = 0; i < y; i++)
+            {
+                char ltr = str[x];
+                Console.Write(ltr);
+                --x;
+            }
+            Console.WriteLine();
+        }
+    }
+}
