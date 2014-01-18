fab82fa5a75ad3638ae95eed8107e0cfe96f0584
2hands <denueg@yahoo.com>
2014-01-18 14:07:24 -0500

    Fibonacci Sequence
    Doesn't calculate past the 46th sequence

---
+++ Fibonacci Sequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static int cur = 1;
+        static int prev = 0;
+        static int temp;
+        static void Main(string[] args)
+        {
+            Console.Write("How many times would you like to calculate the fibonacci sequence? (Up to 46) ");
+            int count = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < count; i++ )
+            {
+                Console.WriteLine(i + ". " + prev);
+                temp = cur + prev;
+                prev = cur;
+                cur = temp;
+            }
+        }
+    }
+}
